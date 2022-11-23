b607a85cf6d5d2b3313eb11022f4c06ff9838966
damian.ruczynski <damian.ruczynski@britenet.com.pl>
2022-11-23 13:29:32 +0100

    handling signup, post data to the API

--- emailclient/src/app/auth/auth.service.ts
+++ emailclient/src/app/auth/auth.service.ts
@@ -5,15 +5,29 @@ interface UsernameAvailableResponse {
   available: boolean;
 }
 
+interface SignupCredentials {
+  password: string;
+  passwordConfirmation: string;
+  username: string;
+}
+
+interface SignupResponse {
+  username: string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  URL = 'https://api.angular-email.com';
 
   constructor(private http: HttpClient) { }
+
   usernameAvailable(username: string) {
-    return this.http.post<UsernameAvailableResponse>('https://api.angular-email.com/auth/username', {
-      username
-    });
+    return this.http.post<UsernameAvailableResponse>(this.URL + '/auth/username', { username });
+  }
+
+  signup(credintials: SignupCredentials | any) {
+    return this.http.post<SignupResponse>(this.URL + '/auth/signup', credintials)
   }
 }
